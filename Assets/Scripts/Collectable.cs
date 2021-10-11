c5998eedccfa2074978f7eb0860a8b123dda02c3
Sfrev <51010839+Sfrev@users.noreply.github.com>
2021-10-11 14:01:08 -0300

    Collectable list

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    private Grimoire grimoire;
+
+    private void Awake()
+    {
+        grimoire = Grimoire.instance;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            grimoire.collectables.Add(this);
             Destroy(gameObject);
         }
     }
